1150ff307bbb5833386ed605a60a3aed5fea7f0c
thuynguyentvt333 <thuynguyen2707yp@gmail.com>
2024-04-22 09:31:01 +0700

    hiển thị danh sách arr obj với react

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,19 +2,29 @@ import React from "react";
 
 class DisplayInfor extends React.Component{
     render(){
-        console.log(this.props);
+        // console.log(this.props);
         //destrucfturing array/object
-        const{name,age}=this.props;
+        const{listUser}=this.props;
 
         //khi dùng cái này thì ở thẻ div ta k cần viết như này <div>My name is {this.props.name}</div>
         //thay vào đó ta có thể viết <div>My name is {name}</div> cho ngắn gọn
-        
+
         //props => properties: tài sản / dùng để truyền dữ liệu cha sang con
         return(
             <div>
-            <div>My name is {this.props.name}</div>
-            <div>My age is {this.props.age}</div>
+                  {listUser.map((user)=>{
+return(
+    <div key={user.id}>
+        <div>My name is {user.name}</div>
+        <div>My age is {user.age}</div>
+    </div>
+)
+})}
             </div>
+            // <div>
+            // <div>My name is {this.props.name}</div>
+            // <div>My age is {this.props.age}</div>
+            // </div>
         )
     }
 }
--- src/components/Mycomponent.js
+++ src/components/Mycomponent.js
@@ -3,16 +3,21 @@ import App from "../App";
 import UserInfor from "./UserInfor";
 import DisplayInfor from "./DisplayInfor";
 class Mycomponent extends React.Component{
-    
+    state={
+        listUser:[
+            {id:1,name:`nguyen`,age:`22` },
+            {id:2, name:`thanh`,age:`23`},
+            {id:3, name:`thuy`,age:`23`}
+        ]
+    }
     //JSX
     render(){
         return(
             <div>
                <UserInfor></UserInfor>
                <br></br>
-               <DisplayInfor name ="Thuy" age="23"></DisplayInfor>
-               <hr/>
-               <DisplayInfor name ="Thuy" age="23"></DisplayInfor>
+
+               <DisplayInfor listUser={this.state.listUser}></DisplayInfor>
             </div>
         );
     }
