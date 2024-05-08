174e77a0861aafcb5b3a7edb1b04bca4c112f3ad
thuynguyentvt333 <thuynguyen2707yp@gmail.com>
2024-05-08 12:39:11 +0700

    video 35 useState Hook

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,54 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import './DisplayIfor.scss';
 import logo from './../logo.svg' ;
-class DisplayInfor extends React.Component{
-    constructor(props){
-        console.log(`call constructor`)
-        super(props);
-        this.state ={
-            isShowListUser: true
-        }
-    }
 
-componentDidMount=()=>{
-console.log(`componentDidMount`);
-setTimeout(()=>{
-    document.title=`test`
-},3000);
+
+const DisplayInfor=(props)=>{
+    const{listUser}=props;
+       
+   const[isShowHideListUser, setShowHideListUser]= useState(true);
+const handleShowHideListUser=()=>{
+setShowHideListUser(!isShowHideListUser);
 }
-componentDidUpdate=(prevProps,prevState)=>{
-    console.log(`componentDidUpdate`,this.props,prevProps);
-    if(this.props.listUser !==prevProps.listUser)
-        if(this.props.listUser.length ==5){
-            alert(`you got 5 member`)
-        }
-}
-
-    handleShowHide=()=>{
-        this.setState({
-            isShowListUser: !this.state.isShowListUser
-
-        })
-    }
-    render(){
-        console.log(`call me render`);
-        //destrucfturing array/object
-        const{listUser}=this.props;
-
-        //khi dùng cái này thì ở thẻ div ta k cần viết như này <div>My name is {this.props.name}</div>
-        //thay vào đó ta có thể viết <div>My name is {name}</div> cho ngắn gọn
 
         //props => properties: tài sản / dùng để truyền dữ liệu cha sang con
         return(
            <div className='display-infor-container'>
-            {/* <img src ={logo}/> */}
-                            <div>
-                            <span onClick={() => {this.handleShowHide()}}>
-                            {this.state.isShowListUser ===true ? "Hide list user" : "Show list user"}
-                            </span>
-                            </div>
+                <div>
+                    <span onClick={()=>{handleShowHideListUser()}}>
+                        {isShowHideListUser===true?"Hide List User":"show list user"}
+                    </span>
+                    </div>            
                            
-{this.state.isShowListUser &&
+{isShowHideListUser &&
             <>
                   {listUser.map((user,index)=>{
                     // console.log(`check: `,user);
@@ -61,7 +33,7 @@ componentDidUpdate=(prevProps,prevState)=>{
                             <div>My age is {user.age}</div>
                             </div>
                             <div>
-                            <button onClick={()=>this.props.handleDeleteUser(user.id)}>Delete </button>
+                            <button onClick={()=>props.handleDeleteUser(user.id)}>Delete </button>
                             </div>
                             <hr/>
                         </div>
@@ -73,6 +45,4 @@ componentDidUpdate=(prevProps,prevState)=>{
             
         )
     }
-}
-
 export default DisplayInfor;
