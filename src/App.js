2a6f560b7f1155a013595a31e604346e9c292caa
thuynguyentvt333 <thuynguyen2707yp@gmail.com>
2024-05-26 06:50:13 +0700

    video 47 trang điều hướng với Nav

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
-import logo from './logo.svg';
+
 import './App.scss';
-import Mycomponent from './components/Mycomponent';
-import React from 'react';
+import Header from './components/Header/Header';
 
-class App extends React.Component{
+const App =() =>{
 
-  render(){
-    return(
-      <div className='app-container'> hello world!
-      <Mycomponent></Mycomponent>
+  return ( 
+    <div className="App-container">
+  <Header/>
     </div>
-    )
-  }
+  );
 }
-// function App() {
-//   return (
-//     <div> hello world!
-//       <Mycomponent></Mycomponent>
-//     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Xin Chao Viet Nam
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-//   );
-// }
 
 export default App;
